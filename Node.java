41c0422304ca3e9417d2e56b805b81f7cae2a326
nganaconda <48596755+nganaconda@users.noreply.github.com>
2019-04-03 17:29:10 +0300

    Add files via upload

--- Node.java
+++ Node.java
@@ -3,9 +3,14 @@ package DS_as1;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface Node
-{
-    List<BrokerImpl> brokers = null;
+public interface Node {
+    List<BrokerImpl> brokers = new ArrayList<BrokerImpl>() {
+        {
+            for(int i = 0; i < 4; i++) {
+                add(new BrokerImpl(Integer.toString(i)));
+            }
+        }
+    };
 
     public void init(int x);
 
